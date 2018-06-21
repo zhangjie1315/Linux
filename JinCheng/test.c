48dab77d07da5583ff35567405b06e1e73fb0882
zhangjie <1466905773@qq.com>
2018-06-21 18:24:26 +0800

    JinCheng/

---
+++ JinCheng/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+{
+	pid_t id = fork();
+	if(id < 0)
+	{
+		perror("fork");
+		return 1;
+	}
+	else if(id > 0 )
+	{
+		//father
+		printf("father [%d] is sleeping...\n",getpid());
+		sleep(30);
+	}
+	else
+	{
+		printf("child [%d] is begin Z...\n",getpid());
+		sleep(5);
+		exit(EXIT_SUCCESS);
+	}
+	return 0;
+}
